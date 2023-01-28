65adb645ae4dfbe9a50614fc5d9ffa6ff2d57194
pushishe <vladvladilin@mail.ru>
2023-01-28 11:35:30 +0300

    fix

--- Kiki/src/store/reducers/search_reducer.ts
+++ Kiki/src/store/reducers/search_reducer.ts
@@ -13,26 +13,24 @@ interface listFilmsAction {
 
 const listFilms = dataListFilms;
 
-export const searchReducer = (
-  state = listFilms,
-  action: listFilmsAction
-) => {
+export const searchReducer = (state = listFilms, action: listFilmsAction) => {
   switch (action.type) {
     case SORT_BY_GENRE:
       return listFilms.filter((film) =>
-        film.genre_ids.includes(action.payload));
+        film.genre_ids.includes(action.payload)
+      );
     case SORT_BY_HIGH:
-      return state.filter((film) =>
-        film.vote_average > 5);
+      return state.filter((film) => film.vote_average > 5);
     case SORT_BY_LOW:
-      return state.filter((film) =>
-        film.vote_average <= 5);
+      return state.filter((film) => film.vote_average <= 5);
     case SORT_BY_POPULAR:
-      return state.filter((film) =>
-        film.popularity > 100 && film.vote_count > 200);
+      return state.filter(
+        (film) => film.popularity > 100 && film.vote_count > 200
+      );
     case SORT_BY_UNKNOWN:
-      return state.filter((film) =>
-        film.popularity < 100 && film.vote_count <= 200);
+      return state.filter(
+        (film) => film.popularity < 100 && film.vote_count <= 200
+      );
     default:
       return state;
   }
