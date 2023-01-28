68ac147428d866132c5118a6524092f58235f135
pushishe <vladvladilin@mail.ru>
2023-01-28 11:34:06 +0300

    Adding fix

--- Kiki/src/components/search/search.tsx
+++ Kiki/src/components/search/search.tsx
@@ -7,93 +7,125 @@ import { useTypedSelector } from "../../hooks/use_typed_selector";
 import "./search.css";
 
 function Search() {
-  const films = useTypedSelector(state => state.searchReducer)
+  const films = useTypedSelector((state) => state.searchReducer);
   const [genre, setGenre] = useState(28);
   const [vote, setVote] = useState("HIGH");
   const [popularity, setPopularity] = useState("POPULAR");
   const [count, setCount] = useState(0);
 
   const nameGenres = () => {
-    const ids = dataListGenres.map(genreOfList => {
-      if (films[count].genre_ids.some(id => id === genreOfList.id)) {
-        return genreOfList.name
+    const ids = dataListGenres.map((genreOfList) => {
+      if (films[count].genre_ids.some((id) => id === genreOfList.id)) {
+        return genreOfList.name;
       }
-    })
-    return ids.filter(name => name !== undefined)
-  }
+    });
+    return ids.filter((name) => name !== undefined);
+  };
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch({type: "GENRE", payload: genre })
-    dispatch({type: vote})
-    dispatch({type: popularity})
-    setCount(0)
+    dispatch({ type: "GENRE", payload: genre });
+    dispatch({ type: vote });
+    dispatch({ type: popularity });
+    setCount(0);
   }, [genre, vote, popularity]);
 
   return (
     <div className="search">
       <div className="form">
-        <select className="select"
-        onChange={(event: React.ChangeEvent<HTMLSelectElement> & {
-          target: string
-        }) => {
-          setGenre(+event.target.value);
-        }}
-        value={genre}>
+        <select
+          className="select"
+          onChange={(
+            event: React.ChangeEvent<HTMLSelectElement> & {
+              target: string;
+            }
+          ) => {
+            setGenre(+event.target.value);
+          }}
+          value={genre}
+        >
           {dataListGenres.map((genre) => {
-            return <option key={genre.id} value={genre.id}>{genre.name}</option>;
+            return (
+              <option key={genre.id} value={genre.id}>
+                {genre.name}
+              </option>
+            );
           })}
         </select>
-        <select className="select" 
-        onChange={(event: React.ChangeEvent<HTMLSelectElement> & {
-          target: string
-        }) => {
-          setVote(event.target.value);
-        }}
-        value={vote}>
+        <select
+          className="select"
+          onChange={(
+            event: React.ChangeEvent<HTMLSelectElement> & {
+              target: string;
+            }
+          ) => {
+            setVote(event.target.value);
+          }}
+          value={vote}
+        >
           <option value="HIGH">Высокая</option>
           <option value="LOW">Низкая</option>
         </select>
-        <select className="select" 
-        onChange={(event: React.ChangeEvent<HTMLSelectElement> & {
-          target: string
-        }) => {
-          setPopularity(event.target.value);
-        }}
-        value={popularity}>
+        <select
+          className="select"
+          onChange={(
+            event: React.ChangeEvent<HTMLSelectElement> & {
+              target: string;
+            }
+          ) => {
+            setPopularity(event.target.value);
+          }}
+          value={popularity}
+        >
           <option value="POPULAR">Популярный</option>
           <option value="UNKNOWN">Неизвестный</option>
         </select>
       </div>
-      {
-        films.length > 0 && count !== films.length  ? (
+      {films.length > 0 && count !== films.length ? (
         <>
-        <div className="film" key={films[count].id}>
-          <div className="poster">
-            <img src={`https://image.tmdb.org/t/p/w500${films[count].poster_path}`} alt="poster" />
-            <div className="overview">
-              <h1>{films[count].title}</h1>
-              <div className="genres">Жанры:{nameGenres().map((name, index) => <p key={index}>{name};</p>)}</div>
-              <p>{films[count].overview}</p>
+          <div className="film" key={films[count].id}>
+            <div className="poster">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${films[count].poster_path}`}
+                alt="poster"
+              />
+              <div className="overview">
+                <h1>{films[count].title}</h1>
+                <div className="genres">
+                  Жанры:
+                  {nameGenres().map((name, index) => (
+                    <p key={index}>{name};</p>
+                  ))}
+                </div>
+                <p>{films[count].overview}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="choice">
-        <button className="no" onClick={() => {
-          setCount(count + 1)
-        }}>Не подходит</button>
-        <Link onClick={() => {
-              dispatch({ type: "ADD_FILM", payload: films[count].id });
-            }} to={`/${films[count].id}`}><button className="yes">Подходит</button></Link>
-      </div>
-      </>
-        ) : <div className="film">
+          <div className="choice">
+            <button
+              className="no"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              Не подходит
+            </button>
+            <Link
+              onClick={() => {
+                dispatch({ type: "ADD_FILM", payload: films[count].id });
+              }}
+              to={`/${films[count].id}`}
+            >
+              <button className="yes">Подходит</button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <div className="film">
           <h1>К сожалению, фильмов не найдено</h1>
         </div>
-      }
-      
-      
+      )}
     </div>
   );
 }
